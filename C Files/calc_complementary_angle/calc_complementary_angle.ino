3b69cfa6bd44a4aeb9b2deedf71ac36507223563
Suchir Srivastava <suchir0311@gmail.com>
2025-03-12 17:30:08 -0700

    applied class_based_rewrite stash to calculate gyroscope tilt angle

--- C Files/calc_complementary_angle/calc_complementary_angle.ino
+++ C Files/calc_complementary_angle/calc_complementary_angle.ino
@@ -2,26 +2,49 @@
 #include "sensor_reading_class.h"
 #include "calculate_tilt_angle_functions.h"
 
-// declare a global SensorReading instance
+// declare global instances of type SensorReading 
 SensorReading sensorInstance;
+SensorReading lastSensorInstance;
 
+// initialize global variables
+int counter = -1;
 float acc_tilt_angle;
 float gyro_tilt_angle;
 float last_gyro_tilt_angle;
-  
+
 void setup() {
+
+  // initialize IMU
   IMU.begin();
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while (1);
-}
+  }
 }
 
 void loop() {
+    
+  counter++;
 
   sensorInstance.readSensors();
   sensorInstance.displaySensorReadings();
-  float acc_tilt_angle = calc_acc_tilt_angle(sensorInstance.ya, sensorInstance.za);
+  acc_tilt_angle = calc_acc_tilt_angle(sensorInstance.ya, sensorInstance.za);
   Serial.println(acc_tilt_angle);
+
+  if(counter % 20 == 0) {
+    gyro_tilt_angle = acc_tilt_angle;
+  }
+  else {
+    gyro_tilt_angle = calc_gyro_tilt_angle(last_gyro_tilt_angle, lastSensorInstance.timestamp, sensorInstance.timestamp, sensorInstance.zg);
+    Serial.println(gyro_tilt_angle);
+  }
+
+  // after all calculations are done
+  if(counter != 1) {
+    lastSensorInstance = sensorInstance;
+    lastSensorInstance.displaySensorReadings();
+  }
+
+  last_gyro_tilt_angle = gyro_tilt_angle;
 
 } 
--- C Files/calc_complementary_angle/calculate_tilt_angle_functions.cpp
+++ C Files/calc_complementary_angle/calculate_tilt_angle_functions.cpp
@@ -7,12 +7,12 @@ float radians_to_degrees(int radians) {
     return radians * 180/PI;
 }
 
-float calc_acc_tilt_angle(int ya, int za) {
+float calc_acc_tilt_angle(int ay, int az) {
     float acc_tilt_angle;
 
-    if(za != 0) {
-        float yzratio = float(ya/za);
-        if ((fabs(za) <=0.04) && (fabs(ya) <=0.04)) {
+    if(az != 0) {
+        float yzratio = float(ay/az);
+        if ((fabs(ay) <=0.04) && (fabs(az) <=0.04)) {
             acc_tilt_angle = radians_to_degrees(yzratio);
         }
         else {
@@ -23,8 +23,8 @@ float calc_acc_tilt_angle(int ya, int za) {
     return acc_tilt_angle;
 }
 
-float calc_gyro_tilt_angle(float last_gyro_angle, float last_timestamp, float current_timestamp, float zg) {
-        float gyro_tilt_angle = last_gyro_angle + ( (current_timestamp - last_timestamp) * zg );
+float calc_gyro_tilt_angle(float last_gyro_angle, float last_timestamp, float current_timestamp, float gz) {
+        float gyro_tilt_angle = last_gyro_angle + ( (current_timestamp - last_timestamp) * gz );
 
     return gyro_tilt_angle;
 }
