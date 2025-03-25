5a8e51128ff7e501f6d34da3d7fb41db5ccfc616
Suchir Srivastava <suchir0311@gmail.com>
2025-03-24 20:05:46 -0700

    created PID tuning branch. Robot currently balancing at small angles

---
+++ C Files/PID_tuning/motor_control_functions.cpp
@@ -0,0 +1,22 @@
+//#include "motor_control_functions.h" 
+//
+//void moveWheelsBackward(int pwmInput) {
+//  analogWrite(pwmA1, 255);
+//  analogWrite(pwmA2, 255-pwmInput);
+//  analogWrite(pwmB1, 255);
+//  analogWrite(pwmB2, 255-pwmInput);
+//}
+//
+//void moveWheelsForward(int pwmInput) {
+//  analogWrite(pwmA1, 255-pwmInput);
+//  analogWrite(pwmA2, 255);
+//  analogWrite(pwmB1, 255-pwmInput);
+//  analogWrite(pwmB2, 255);
+//}
+//
+//void stopWheels() {
+//  analogWrite(pwmA1, 255);
+//  analogWrite(pwmA2, 255);
+//  analogWrite(pwmB1, 255);
+//  analogWrite(pwmB2, 255);
+//}
---
+++ C Files/PID_tuning/motor_control_functions.h
@@ -0,0 +1,15 @@
+//#ifndef MOTOR_CONTROL_FUNCTIONS_H
+//#define MOTOR_CONTROL_FUNCTIONS_H
+//
+//#include "Arduino_BMI270_BMM150.h"
+//
+//const int pwmA1 = 9;
+//const int pwmA2 = 10;
+//const int pwmB1 = 5;
+//const int pwmB2 = 6;
+//
+//void moveWheelsBackward(int pwmInput);
+//void moveWheelsForward(int pwmInput);
+//void stopWheels();
+//
+//#endif
